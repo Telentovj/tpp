6e38e327fbf2bf15d9a1b67ef795214003bc1844
john.vijay <john.vijay@motional.com>
2022-10-14 17:24:31 +0800

    double click err solved

--- front-end/main.py
+++ front-end/main.py
@@ -1,10 +1,9 @@
-from distutils.command.upload import upload
 import streamlit as st
 import time
-from models.topic_models import *
+# from models.topic_models import *
 
-with open( "styles.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+with open("styles.css") as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
 if "currentPage" not in st.session_state:
     st.session_state.currentPage = "mainPage"
@@ -18,15 +17,17 @@ insightPage = st.empty()
 def change_page(page):
     st.session_state.currentPage = page
 
-# Main page 
+
+# Main page
 if st.session_state.currentPage == "mainPage":
-    mainPage = st.container();
+    mainPage = st.container()
     with mainPage:
         col1, col2, col3 = st.columns([0.5, 0.1, 0.5])
         col2.image("csv-svgrepo-com 1csv.png", use_column_width=True)
-        st.markdown("<h2 style='text-align: center;font-size: 24px;'>Preprocess your text data</h2>", unsafe_allow_html=True)
+        st.markdown(
+            "<h2 style='text-align: center;font-size: 24px;'>Preprocess your text data</h2>", unsafe_allow_html=True)
 
-        uploaded_file = st.file_uploader("",key="enabled")
+        uploaded_file = st.file_uploader("", key="enabled")
 
         if uploaded_file is not None:
             my_bar = st.progress(0)
@@ -35,29 +36,17 @@ if st.session_state.currentPage == "mainPage":
                 my_bar.progress(percent_complete + 1)
                 if percent_complete == 99:
                     my_bar.empty()
-                    col1, col2, col3 = st.columns([1,1,1])
+                    col1, col2, col3 = st.columns([1, 1, 1])
                     st.markdown("""---""")
-                    st.session_state.currentPage = "insightPage"
-                    insight = col2.button("Click here to focus on the insights that has be found!", key="insight")
-                    if insight:
-                        st.session_state.currentPage = "insightPage"
+                    insight = col2.button("Click here to focus on the insights that has be found!",
+                        on_click=change_page, args=("insightPage", ))
+        
+        format_btn = st.markdown(
+            "<h3 style='text-align: center; font-size: 14px;'>Unsure about how to format your text data?<h3>", unsafe_allow_html=True)
+        col1, col2, col3 = st.columns([1, 1, 1])
 
-                    
-        format_btn = st.markdown("<h3 style='text-align: center; font-size: 14px;'>Unsure about how to format your text data?<h3>", unsafe_allow_html=True)
-        col1, col2, col3 = st.columns([1,1,1])
-
-        faq = col2.button("Read our FAQs for a quick guide!", on_click=change_page, args=("faqPage", ))
-        # if faq:
-        #     print(f"ran in if {st.session_state.currentPage}")
-        #     st.session_state.currentPage = "faqPage"
-        #     print(f"ran in if {st.session_state.currentPage}")
-        #     st.write('Click again to focus on the Faqs!')
-        #     st.markdown("""---""")
-
-
-
-
-
+        faq = col2.button("Read our FAQs for a quick guide!",
+                          on_click=change_page, args=("faqPage", ))
 
 
 # FAQ page
@@ -69,32 +58,31 @@ if st.session_state["currentPage"] is "faqPage":
             ('How to format my excel file?', 'How to do that?', 'How to do those?'))
         st.write(option)
         if option == 'How to format my excel file?':
-            st.write('Answer for: '+ option)
+            st.write('Answer for: ' + option)
         if option == 'How to do that?':
-            st.write('Answer for: '+ option)
+            st.write('Answer for: ' + option)
         if option == 'How to do those?':
-            st.write('Answer for: '+ option)
-        
-        if st.button("Close Faqs"):
-            st.write("Are you sure you want to leave the Faqs?")
-            st.session_state.currentPage = "mainPage"
+            st.write('Answer for: ' + option)
+
+        close_faq = st.button("Close Faqs",
+                          on_click=change_page, args=("mainPage", ))
+
 
 
 # Insights page
 
 if st.session_state["currentPage"] is "insightPage":
-    
+
     insightPage = st.container()
     with insightPage:
         option = st.selectbox(
             'What do you want to know about your text file?',
-            ('Select a question','What are some topics found in the data?', 'How many times did each identified topic appear?'))
+            ('Select a question', 'What are some topics found in the data?', 'How many times did each identified topic appear?'))
 
         if option == 'What are some topics found in the data?':
-            st.write('Answer for: '+ option)
+            st.write('Answer for: ' + option)
         if option == 'How many times did each identified topic appear?':
-            st.write('Answer for: '+ option)
-        
-        if st.button("Input another file"):
-            st.write("Are you sure you want to leave the page?")
-            st.session_state.currentPage = "mainPage"
+            st.write('Answer for: ' + option)
+
+        go_back = st.button("Input another file",
+                on_click=change_page, args=("mainPage", ))
