599df2899950edcbe2bba1c6b2c8abaa4cec9221
Jegan <jeyakumarjegan123@gmail.com>
2022-10-19 16:53:19 +0800

    editied top2vec codes

--- back-end/topic_models.py
+++ back-end/topic_models.py
@@ -221,13 +221,12 @@ def lda_display(lda_model, bow_corpus, dictionary):
 ################################################################
 from top2vec import Top2Vec
 
-# docs is a array of strings, numTopics is an int
-# returns an array of array with top words for all topics
-top2VecModel = None
+#docs is a string array of documents, numTopic is an integer
+def runTop2Vec(docs):
+    return Top2Vec(docs)
 
-def runTop2Vec(docs, numTopics):
-    tempTop2VecModel = Top2Vec(docs)
-    top2VecModel = tempTop2VecModel.hierarchical_topic_reduction(numTopics)
+def runTop2VecReduced(model, numTopics):
+     return model.hierarchical_topic_reduction(numTopics)
 
 # get topic words for all topics
 def getTopicWords(model):
@@ -235,9 +234,22 @@ def getTopicWords(model):
 
 # print all wordclouds
 def printWordCloud(model, numTopic):
-    for i in range(numTopic + 1):
+    for i in range(numTopic):
         Top2Vec.generate_topic_wordcloud(
             model, i, background_color="black", reduced=True)
+        
+import seaborn as sns
+#print topic word score barchart
+def printWordBar(model, numTopic):
+  for i in range(numTopic):
+    topic_names = model.topic_words_reduced[i]#[:5]
+    topic_probs = model.topic_word_scores_reduced[i]#[:5]
+    df_topics = pd.DataFrame(topic_names).rename(columns={0 : "Topic Words"})
+    df_probs = pd.DataFrame(topic_probs).rename(columns={0 : "Probability"})
+    df = pd.concat([df_topics, df_probs], axis=1)
+    plt.figure()
+    sns.set(rc={'figure.figsize':(11.7,12)}) 
+    ax = sns.barplot(x="Probability", y="Topic Words", data=df, palette="mako").set(title='Topic ' + str(i))
 
 ################################################################
 #                           BERTopic                          #
@@ -278,7 +290,7 @@ def get_topic_freq(model):
     """
         Return the size of the topics (in descending order)
     """
-    return mode.get_topic_freq()
+    return model.get_topic_freq()
 
 def visualize_barchart_bert(model):
     """
