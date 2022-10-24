daa4799ed0796a6752788eb0ce79855537e8f622
Telentovj <johnvijaybalasupramaniam@gmail.com>
2022-10-24 20:04:58 +0800

    remove burger menu, wu chuan graph logic

--- main.py
+++ main.py
@@ -16,10 +16,14 @@ st.title("Text Pre Processing")
 mainPage = st.empty()
 faqPage = st.empty()
 insightPage = st.empty()
-
+downloadPage = st.empty()
 
 def change_page(page):
     st.session_state.currentPage = page
+
+def set_topic_model(model):
+    st.session_state.topicModel = model
+    change_page("downloadPage")
 
 
 # Main page
@@ -30,23 +34,32 @@ if st.session_state.currentPage == "mainPage":
         col2.image("csv.png", use_column_width=True)
         st.markdown(
             "<h2 style='text-align: center;font-size: 24px;'>Preprocess your text data</h2>", unsafe_allow_html=True)
-
+        
+        # Faqs
+        col1, col2, col3 = st.columns([1, 1, 1])
+        faq = col2.button("Read our FAQs for a quick guide!",
+                          on_click=change_page, args=("faqPage", ))
+        
+        # File uploader
         uploaded_file = st.file_uploader("", key="enabled")
 
+        # add logic to ensure that number of topics is not None
         if uploaded_file is not None:
             df = load_data(uploaded_file)
             df, docs, docs_tokenized = preprocess_data(df)
             st.session_state["dataframe"] = df
-            bert = run_bertopic(docs, 3)
-            samples = get_top_documents(df, bert, 3, 3)
-            csv = samples_to_csv(samples)
+            
+            bert = run_bertopic(docs, 4)
+            st.session_state["bert"] = bert
+            # samples = get_top_documents(df, bert, 3, 3)
+            # csv = samples_to_csv(samples)
 
-            st.download_button(
-                label="Download data as CSV",
-                data=csv,
-                file_name='test.csv',
-                mime='text/csv',
-            )
+            # st.download_button(
+            #     label="Download data as CSV",
+            #     data=csv,
+            #     file_name='test.csv',
+            #     mime='text/csv',
+            # )
 
             my_bar = st.progress(0)
             for percent_complete in range(100):
@@ -54,15 +67,12 @@ if st.session_state.currentPage == "mainPage":
                 my_bar.progress(percent_complete + 1)
                 if percent_complete == 99:
                     my_bar.empty()
-                    col1, col2, col3 = st.columns([1, 1, 1])
                     st.markdown("""---""")
+                    col1, col2, col3 = st.columns([1, 1, 1])
                     insight = col2.button("Click here to focus on the insights that has be found!",
                                           on_click=change_page, args=("insightPage", ))
 
-        col1, col2, col3 = st.columns([1, 1, 1])
 
-        faq = col2.button("Read our FAQs for a quick guide!",
-                          on_click=change_page, args=("faqPage", ))
 
 
 # FAQ page
@@ -88,15 +98,41 @@ if st.session_state["currentPage"] == "faqPage":
 
 if st.session_state["currentPage"] == "insightPage":
     insightPage = st.container()
+
     with insightPage:
-        option = st.selectbox(
-            'What do you want to know about your text file?',
-            ('Select a question', 'What are some topics found in the data?', 'How many times did each identified topic appear?'))
+        
+        #BERT
+        bert = st.session_state['bert']
+        bert_expander = st.expander("Bert")
+        bert_expander.write(bert.visualize_barchart().update_layout(autosize=False,width = 670,height=400))
 
-        if option == 'What are some topics found in the data?':
-            st.write('Answer for: ' + option)
-        if option == 'How many times did each identified topic appear?':
-            st.write('Answer for: ' + option)
+        #Top2Vec
+        Top2Vec_expander = st.expander("Top2Vec")
+                
 
+        #LDA 
+        LDA_expander = st.expander("LDA")
+
+
+        #NMF
+        NML_expander = st.expander("NMF")
+
+
+        col1, col2, col3, col4 = st.columns([0.25,0.25,0.25,0.25])
+        generate_with_a = col1.button("Generate dataset with Bert",
+                            on_click=set_topic_model, args=("bert", ))
+        generate_with_b = col2.button("Generate dataset with Top2Vec",
+                            on_click=set_topic_model, args=("top2vec", ))
+        generate_with_c = col3.button("Generate dataset with LDA",
+                            on_click=set_topic_model, args=("lda", ))
+        generate_with_c = col4.button("Generate dataset with NMF",
+                            on_click=set_topic_model, args=("nmf", ))
+
+# Download Page
+
+if st.session_state["currentPage"] == "downloadPage":
+    downloadPage = st.container()
+    with downloadPage:
+        st.write(st.session_state["topicModel"])
         go_back = st.button("Input another file",
                             on_click=change_page, args=("mainPage", ))
