5fd54ef5acf8739c7f8315443480a2fd2d2a802a
Telentovj <johnvijaybalasupramaniam@gmail.com>
2022-10-24 20:37:38 +0800

    number of topics added and error handling for it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import math
 import time
 from topic_models.data import *
 from topic_models.bertopic import *
@@ -36,41 +37,63 @@ if st.session_state.currentPage == "mainPage":
             "<h2 style='text-align: center;font-size: 24px;'>Preprocess your text data</h2>", unsafe_allow_html=True)
         
         # Faqs
-        col1, col2, col3 = st.columns([1, 1, 1])
-        faq = col2.button("Read our FAQs for a quick guide!",
+        col1, col2, col3 = st.columns([1, 0.55, 1])
+        faq = col2.button("Read our FAQs!",
                           on_click=change_page, args=("faqPage", ))
         
+        # Input for number of topics 
+        number_of_topics = st.number_input(
+            'Insert number of Topics, decimals will be rounded down.',
+            min_value = 0, 
+            max_value= 10,
+        )
+
         # File uploader
         uploaded_file = st.file_uploader("", key="enabled")
 
         # add logic to ensure that number of topics is not None
         if uploaded_file is not None:
-            df = load_data(uploaded_file)
-            df, docs, docs_tokenized = preprocess_data(df)
-            st.session_state["dataframe"] = df
+            if number_of_topics:
+                number_of_topics = math.floor(number_of_topics)
+                df = load_data(uploaded_file)
+                df, docs, docs_tokenized = preprocess_data(df)
+                st.session_state["dataframe"] = df
+                
+                # Bert logic
+                bert = run_bertopic(docs, 4)
+                st.session_state["bert"] = bert
+                # samples = get_top_documents(df, bert, 3, 3)
+                # csv = samples_to_csv(samples)
+
+                # st.download_button(
+                #     label="Download data as CSV",
+                #     data=csv,
+                #     file_name='test.csv',
+                #     mime='text/csv',
+                # )
+
+                # Lda logic
+                lda = run_lda(docs,4)
+
+                # nmf logic
+                # nmf = run_nmf(docs,4)
+
+                # top2vec logic
+                # top2vec = runTop2Vec(docs)
+
+                my_bar = st.progress(0)
+                for percent_complete in range(100):
+                    time.sleep(0.005)
+                    my_bar.progress(percent_complete + 1)
+                    if percent_complete == 99:
+                        my_bar.empty()
+                        st.markdown("""---""")
+                        col1, col2, col3 = st.columns([1, 1, 1])
+                        insight = col2.button("Click here to focus on the insights that has be found!",
+                                            on_click=change_page, args=("insightPage", ))
+            else:
+                st.warning('Please insert the number of topics.')
             
-            bert = run_bertopic(docs, 4)
-            st.session_state["bert"] = bert
-            # samples = get_top_documents(df, bert, 3, 3)
-            # csv = samples_to_csv(samples)
-
-            # st.download_button(
-            #     label="Download data as CSV",
-            #     data=csv,
-            #     file_name='test.csv',
-            #     mime='text/csv',
-            # )
-
-            my_bar = st.progress(0)
-            for percent_complete in range(100):
-                time.sleep(0.005)
-                my_bar.progress(percent_complete + 1)
-                if percent_complete == 99:
-                    my_bar.empty()
-                    st.markdown("""---""")
-                    col1, col2, col3 = st.columns([1, 1, 1])
-                    insight = col2.button("Click here to focus on the insights that has be found!",
-                                          on_click=change_page, args=("insightPage", ))
 
 
 
@@ -108,7 +131,7 @@ if st.session_state["currentPage"] == "insightPage":
 
         #Top2Vec
         Top2Vec_expander = st.expander("Top2Vec")
-                
+
 
         #LDA 
         LDA_expander = st.expander("LDA")
